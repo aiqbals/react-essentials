fbc157b2f6c898c55eb509d67191b7739ee5e52e
Asif Hoque <asif.engr13@gmail.com>
2021-04-23 13:25:32 +0200

    test

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//import App from './App';
+//import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
+  /* <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode> */
+  // React.StrictMode is used to warn when there is violation of react bestpractices
+  React.createElement('h1', {style: {color: 'red'}}, 'Hello world'),
   document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals();
