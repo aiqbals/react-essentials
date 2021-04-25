6b56ecb0000095578fce3d17ccbf1e9ae12b17c9
Asif Hoque <asif.engr13@gmail.com>
2021-04-25 17:37:48 +0200

    test

--- src/Checkbox.js
+++ src/Checkbox.js
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 
 function Checkbox() {
-    <input type='checkbox' />
+    const [checked, toggle] = useReducer(
+        (checked) => !checked,
+        false
+    );
+    return (
+        <>
+            <label> {checked ? 'checked' : 'not checked'} </label>
+            <input type='checkbox' />
+        </>
+    )
 }
 export  default Checkbox;
